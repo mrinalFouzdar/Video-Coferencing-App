4a6fac76691c0cb37f1fe219df3eef3152b0078a
mrinalFouzdar <100467560+mrinalFouzdar@users.noreply.github.com>
2022-06-14 15:01:40 +0530

    Total Time

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   useHMSStore
 } from "@100mslive/react-sdk";
 import Header from './Components/Header';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Conference from './Components/Conference';
 import Footer from './Components/Footer';
 import JoinFrom from './Components/JoinForm';
@@ -14,13 +14,20 @@ import JoinFrom from './Components/JoinForm';
 function App() {
   const isConnected = useHMSStore(selectIsConnectedToRoom);
   const hmsActions = useHMSActions();
+  // const [time,setTime]=useState(0)
   useEffect(()=>{
+    // if(isConnected){
+    //   let now =new Date()
+    //   setTime(now.toLocaleString)
+    // }
     window.onunload = () => {
       if (isConnected) {
         hmsActions.leave();
       }
     };
   },[isConnected,hmsActions])
+  // console.log(time)
+  console.log(new Date().getTime())
   return (
     <div className="App">
      <Header/>
--- src/Components/Conference.jsx
+++ src/Components/Conference.jsx
@@ -1,9 +1,21 @@
 import { selectPeers, useHMSStore } from "@100mslive/react-sdk";
+import { useEffect } from "react";
 import Peer from "./Peer";
 
 function Conference() {
   const peers = useHMSStore(selectPeers);
-  // console.log(peers.joinedAt())
+  useEffect(()=>{
+    let time = Date.now()
+    return ()=>{
+      let total_time = Date.now()-time
+      const seconds = (total_time / 1000);
+      const minutes = (seconds / 60).toFixed(2);
+      if(minutes> 0.02){
+        alert(`Total Time 🕛: ${minutes} Minutes `)
+
+      }
+    }
+  },[])
     
   return (
     <div className="conference-section">
